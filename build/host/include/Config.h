5d7f0174bd565646610e64e9d0f8d82e4ccf2d44
bsantella <bsantella1410@gmail.com>
2023-04-04 03:27:45 -0700

    fixed to work on my end

--- build/host/include/Config.h
+++ build/host/include/Config.h
@@ -257,7 +257,7 @@
 #define VERSION_SUFFIX ""
 #define VERSION "1.0.3"
 #define VERSIONCODE 65539
-#define RELEASE "1.0.3-git(3e595c6)*"
+#define RELEASE "1.0.3-git(2cbf026)*"
 #define FREENOS 1
 #define COMPILER_VERSION "gcc (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 #define BUILDUSER "bryson"
